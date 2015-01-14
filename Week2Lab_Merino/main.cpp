b0e3a4d6fca8457aeaec2e79a01f6428ba1f2bbb
Kim Merino <dafabulousteach@gmail.com>
2015-01-13 23:29:25 -0800

    Initial Commit

---
+++ Week2Lab_Merino/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Week2Lab_Merino
+//
+//  Created by Big Brother on 1/13/15.
+//  Copyright (c) 2015 CIS247C. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
